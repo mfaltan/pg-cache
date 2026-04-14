a73dcad3fbe09aac5ceaae7305084d4f89dd0df9
Miloslav Faltan <mfaltan@gmail.com>
2026-04-14 23:21:55 +0200

    32-init-table

--- pg-cache-spring-boot-autoconfigure/src/main/java/io/github/mfaltan/pgcache/config/CacheConfig.java
+++ pg-cache-spring-boot-autoconfigure/src/main/java/io/github/mfaltan/pgcache/config/CacheConfig.java
@@ -2,8 +2,11 @@ package io.github.mfaltan.pgcache.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.mfaltan.pgcache.core.*;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.AbstractCachingConfiguration;
 import org.springframework.cache.interceptor.CacheInterceptor;
@@ -13,12 +16,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Role;
 
+import javax.sql.DataSource;
+
 @Configuration
+@EnableConfigurationProperties(PgCacheProperties.class)
 public class CacheConfig extends AbstractCachingConfiguration {
 
+    @Bean(name = "pgCacheAdminDataSource")
+    public DataSource pgCacheAdminDataSource(PgCacheProperties properties) {
+        PgCacheProperties.DataSourceProperties props = properties.getAdminDatasource();
+        PGSimpleDataSource ds = new org.postgresql.ds.PGSimpleDataSource();
+        ds.setURL(props.getUrl());
+        ds.setUser(props.getUsername());
+        ds.setPassword(props.getPassword());
+        return ds;
+    }
+
     @Bean
-    StoreFactory storeFactory(){
-        return new RamStoreFactory();
+    StoreFactory storeFactory(PgCacheProperties properties,
+                              @Qualifier("pgCacheAdminDataSource") DataSource adminDataSource){
+        return PgStoreFactory.builder()
+                             .adminDataSource(adminDataSource)
+                             .tableName(properties.getTableName())
+                             .build();
     }
 
     @Bean
