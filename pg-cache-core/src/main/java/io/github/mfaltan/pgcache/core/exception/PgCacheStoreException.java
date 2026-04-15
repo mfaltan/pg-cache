4f62fc0340f9f1dac6cf1035be55da0efc88b22e
mfaltan <mfaltan@gmail.com>
2026-04-15 21:25:49 +0200

    46-custom-exception-if-db-operation-failed (#52)

---
+++ pg-cache-core/src/main/java/io/github/mfaltan/pgcache/core/exception/PgCacheStoreException.java
@@ -0,0 +1,7 @@
+package io.github.mfaltan.pgcache.core.exception;
+
+public class PgCacheStoreException extends PgCacheException{
+    public PgCacheStoreException(String message, Exception cause) {
+        super(message, cause);
+    }
+}
