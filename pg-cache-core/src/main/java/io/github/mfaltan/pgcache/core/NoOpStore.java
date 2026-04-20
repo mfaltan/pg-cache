2cd5ace4fd91894b6d5b36c87669566458cdde12
Miloslav Faltan <mfaltan@gmail.com>
2026-04-20 17:26:41 +0200

    sonar

--- pg-cache-core/src/main/java/io/github/mfaltan/pgcache/core/NoOpStore.java
+++ pg-cache-core/src/main/java/io/github/mfaltan/pgcache/core/NoOpStore.java
@@ -8,22 +8,27 @@ public class NoOpStore implements Store {
 
     @Override
     public void put(Long key, CacheEntry entry) {
+        //do nothing
     }
 
     @Override
     public void remove(Long key) {
+        //do nothing
     }
 
     @Override
     public void clear() {
+        //do nothing
     }
 
     @Override
     public CacheEntry get(Long key) {
+        //do nothing
         return null;
     }
 
     @Override
     public void evictExpired(int limit) {
+        //do nothing
     }
 }
