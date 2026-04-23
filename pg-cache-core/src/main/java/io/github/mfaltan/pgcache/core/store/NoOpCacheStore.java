a9f6e38dcb18b46a6b943687d65662e64b3b2b61
mfaltan <mfaltan@gmail.com>
2026-04-24 00:21:18 +0200

    49-add-possibility-to-turn-onoff-specific-cache-via-config (#98)

--- pg-cache-core/src/main/java/io/github/mfaltan/pgcache/core/store/NoOpCacheStore.java
+++ pg-cache-core/src/main/java/io/github/mfaltan/pgcache/core/store/NoOpCacheStore.java
@@ -3,38 +3,45 @@ package io.github.mfaltan.pgcache.core.store;
 import io.github.mfaltan.pgcache.common.Constants;
 import io.github.mfaltan.pgcache.core.domain.CacheEntry;
 import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@RequiredArgsConstructor
 @EqualsAndHashCode
 @Slf4j
 public class NoOpCacheStore implements CacheStore {
 
+    private final boolean temporary;
 
     @Override
     public void put(Long key, CacheEntry entry) {
-        log.debug(Constants.MARKER, "Not storing key [{}], caching temporarily disabled", key);
+        log.debug(Constants.MARKER, "Not storing key [{}], caching {} disabled", key, getMessage());
     }
 
     @Override
     public void remove(Long key) {
-        log.debug(Constants.MARKER, "Not evicting key [{}], caching temporarily disabled", key);
+        log.debug(Constants.MARKER, "Not evicting key [{}], caching {} disabled", key, getMessage());
 
     }
 
     @Override
     public void clear() {
-        log.debug(Constants.MARKER, "Not clearing cache, caching temporarily disabled");
+        log.debug(Constants.MARKER, "Not clearing cache, caching {} disabled", getMessage());
 
     }
 
     @Override
     public CacheEntry get(Long key) {
-        log.debug(Constants.MARKER, "Not getting key [{}], caching temporarily disabled", key);
+        log.debug(Constants.MARKER, "Not getting key [{}], caching {} disabled", key, getMessage());
         return null;
     }
 
     @Override
     public void evictExpired(int limit) {
-        log.debug(Constants.MARKER, "Not evicting [{}] expired keys, caching temporarily disabled", limit);
+        log.debug(Constants.MARKER, "Not evicting [{}] expired keys, caching {} disabled", limit, getMessage());
+    }
+
+    private String getMessage() {
+        return temporary ? "temporarily" : "";
     }
 }
